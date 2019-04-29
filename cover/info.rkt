codecov/codecov-racket

add support for adding new build services

cover/info.rkt
@@ -1,3 +1,9 @@
 #lang setup/infotab
 
 (define cover-formats '(("codecov" cover/codecov generate-codecov-coverage)))
+(define cover-build-services '(("gitlab-service" cover/private/gitlab-service
+                                                 gitlab-service?
+                                                 gitlab-service@)
+                               ("travis-service" cover/private/travis-service
+                                                 travis-service?
+                                                 travis-service@)))

florence/cover-codecov

cover/info.rkt
@@ -1,3 +1,9 @@
 #lang setup/infotab
 
 (define cover-formats '(("codecov" cover/codecov generate-codecov-coverage)))
+(define cover-build-services '(("gitlab-service" cover/private/gitlab-service
+                                                 gitlab-ci?
+                                                 gitlab-service@)
+                               ("travis-service" cover/private/travis-service
+                                                 travis-ci?
+                                                 travis-service@)))